44c4d20ae60411490bd1060116b3f315d07528b5
toukali <141361989+toukali@users.noreply.github.com>
2024-06-29 11:59:10 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -12,18 +12,29 @@ def pascal_triangle(n):
         Returns:
             pascal_triangle (list): Binary string of the sum of a and b
     '''
-    if n == 1 :
-        return [[1]]
-    if n<=0 :
-        return list()
-    else:
-        result = pascal_triangle(n - 1)
-        result.append([])
-        for i in range(n) :
-            if i == 0:
-                result[n - 1].append(1)
-            elif i == n - 1:
-                result[n - 1].append(1)
-            else:
-                result[n - 1].append(result[n - 2][i] + result[n - 2][i - 1])
-    return result
+    pascal_triangle = list()
+
+    if n <= 0:
+        return pascal_triangle
+
+    # Add first 1.
+    if n > 0:
+        pascal_triangle.append([1])
+
+    # Add second line.
+    if n > 1:
+        pascal_triangle.append([1, 1])
+
+    for x in range(3, n+1):
+        pascal_triangle.append([0] * x)
+
+        # Set first and last 1
+        pascal_triangle[x-1][0] = 1
+        pascal_triangle[x-1][x-1] = 1
+
+        # Calculate middle numbers
+        for y in range(1, x-1):
+            pascal_triangle[x-1][y] = \
+                pascal_triangle[x-2][y-1] + pascal_triangle[x-2][y]
+
+    return pascal_triangle
