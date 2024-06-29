0c0b78ba1c10e09af8270a978b357cc90cd394d3
toukali <141361989+toukali@users.noreply.github.com>
2024-06-29 11:09:35 +0100

    Create 0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+1#!/usr/bin/python3
+def pascal_triangle(n):
+    if n == 1:
+        return [[1]]
+    if n<=0:
+        return []
+    else:
+        result = pascal_triangle(n-1)
+        result.append([])
+        for i in range(n):
+            if i == 0:
+                result[n-1].append(1)
+            elif i == n-1:
+                result[n-1].append(1)
+            else:
+                result[n-1].append(result[n-2][i]+result[n-2][i-1])
+        return result
