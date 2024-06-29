1189ee8b21fd2df5c0e966735205e4116c0602b4
toukali <141361989+toukali@users.noreply.github.com>
2024-06-29 11:52:27 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -17,13 +17,13 @@ def pascal_triangle(n):
     if n<=0 :
         return list()
     else:
-        result = pascal_triangle(n-1)
+        result = pascal_triangle(n - 1)
         result.append([])
         for i in range(n) :
             if i == 0:
-                result[n-1].append(1)
-            elif i == n-1:
-                result[n-1].append(1)
+                result[n - 1].append(1)
+            elif i == n - 1:
+                result[n - 1].append(1)
             else:
-                result[n-1].append(result[n-2][i]+result[n-2][i-1])
+                result[n - 1].append(result[n - 2][i] + result[n - 2][i - 1])
     return result
