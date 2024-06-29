93308427f55f621f21cbd8f9601a88a28419acac
toukali <141361989+toukali@users.noreply.github.com>
2024-06-29 11:16:34 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
+'''Module to find Pascal's Triangle integers'''
+
+
 def pascal_triangle(n):
+        '''
+    Function to find Pascal's Triangle integers
+
+        Parameters:
+            n (int): The number of row's of Pascal's triangle
+
+        Returns:
+            pascal_triangle (list): Binary string of the sum of a and b
+    '''
+    # Add first 1.
     if n == 1:
         return [[1]]
+     # Add emty list.
     if n<=0:
         return []
     else:
@@ -14,4 +28,4 @@ def pascal_triangle(n):
                 result[n-1].append(1)
             else:
                 result[n-1].append(result[n-2][i]+result[n-2][i-1])
-        return result
+    return result
